9dc76177463da178a0de589b597b6ba00caba35f
ritchie46 <ritchie46@gmail.com>
2019-07-11 09:00:48 +0200

    refactor settlement

--- pressure/settlement.py
+++ pressure/settlement.py
@@ -34,13 +34,40 @@ def settlement_over_depth(cs, cp, depth, sigma, pile_tip_level=None, t=10000, p=
     # evaluate values ot pile tip level
     cp = cp[:pile_tip_level]
     cs = cs[:pile_tip_level]
+
+    # TODO: unit conversion?
     sigma = sigma[:pile_tip_level] * 1e3
-    c_factor = 4.
 
-    eps = (1./(cp * c_factor) + 1./(cs * c_factor) * np.log10(t)) * np.log(sigma * ocr / sigma) + \
-          (1. / cp + 1. / cs * np.log10(t)) * np.log((sigma + p) / (sigma * ocr))
-
-    eps = np.nan_to_num(eps)
+    eps = np.nan_to_num(koppejan(cs, cp, sigma, t, p, ocr))
 
     dh = eps[:pile_tip_level - 1] * np.diff(depth[:pile_tip_level])
     return np.cumsum(dh[::-1])[::-1]
+
+
+def koppejan(cs, cp, sigma, t=10000, p=2., ocr=1.):
+    """
+    Koppejan settlement strain.
+
+    Parameters
+    ----------
+    cs : float
+        C's; secondary stiffness over depth
+    cp : float
+        C'p; primary stiffness over depth.
+    sigma : float
+        Grain pressure
+    t : int
+        Time in days.
+    p : float
+        Load in MPa.
+    ocr : float
+        Over consolidation ratio.
+
+    Returns
+    -------
+    eps : float
+        strain
+    """
+    c_factor = 4.
+    return (1. / (cp * c_factor) + 1. / (cs * c_factor) * np.log10(t)) * np.log(sigma * ocr / sigma) + \
+           (1. / cp + 1. / cs * np.log10(t)) * np.log((sigma + p) / (sigma * ocr))
