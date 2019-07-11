e04786dc148e2be94ee9abba5d8fa5196563db20
ritchie46 <ritchie46@gmail.com>
2019-07-11 16:41:33 +0200

    refactor

--- pressure/bearing.py
+++ pressure/bearing.py
@@ -1,6 +1,76 @@
 import numpy as np
 
 
+def compute_pile_tip_resistance(ptl, qc, depth, d_eq, alpha, beta, s, A, return_q_components=False):
+    """
+
+    Parameters
+    ----------
+    ptl : float
+        Pile tip level
+    qc : array
+        CPT's qc values
+    depth : array
+        Depth of cpt w/ respect to ground level
+    d_eq : float
+        Equivalent diameter pile.
+    alpha : float
+    beta : float
+    s : float
+    A : float
+        Pile tip area.
+    return_q_components : bool
+        Return separate qI, qII, qIII tracks
+
+    Returns
+    -------
+    rb : float
+        Resistance at pile tip.
+
+    """
+    d07 = 0.7 * d_eq + ptl
+    d4 = 4 * d_eq + ptl
+    d8 = ptl - 8 * d_eq
+
+    # closest indices
+    ppni = np.argmin(np.abs(ptl - depth))
+    d07i = np.argmin(np.abs(d07 - depth))
+    d4i = np.argmin(np.abs(d4 - depth))
+    d8i = np.argmin(np.abs(d8 - depth))
+
+    # range from ppn to 4d
+    qc_range_ppn_4d = qc[ppni:d4i + 1]
+
+    # cumulative min from 4d back to ppn
+    qc2_range = np.fmin.accumulate(qc_range_ppn_4d[::-1])[::-1]
+
+    # array used for determining the minimal rb-max. The minimal mean of both q1 and q2 are decisive
+    qc_calc = qc2_range + qc_range_ppn_4d
+    qc_calc_mean_range = qc_calc.cumsum() / np.arange(1, qc_calc.shape[0] + 1)
+
+    # index of the minimal mean of both qc1 and qc2
+    di = d07i - ppni
+
+    min_idx = np.argmin(qc_calc_mean_range[di:])
+    idx = min_idx + di if min_idx > 0 else min_idx + di + 1
+
+    # now the actual mean can be determined
+    qc1 = np.mean(qc_range_ppn_4d[:idx])
+    qc2 = np.mean(qc2_range[:idx])
+
+    # qc3 starts where qc2 is ended and accumulates the minimum again.
+    qc3_range = qc[d8i: ppni + 1].copy()
+    qc3_range[-1] = qc2_range[0]
+    qc3_range = np.fmin.accumulate(qc3_range[::-1])[::-1]
+    qc3 = np.mean(qc3_range)
+
+    rb = min(alpha * beta * s * 0.5 * (0.5 * (qc1 + qc2) + qc3), 15) * A
+
+    if return_q_components:
+        return rb, qc1, qc2, qc3
+    return rb
+
+
 def chamfer_positive_friction(qc, depth):
     """
     Parameters
@@ -86,73 +156,4 @@ def positive_friction(depth, chamfered_qc, circum, alpha_s=0.01):
     force = shaft_stress[:-1] * np.diff(depth)
     return np.append(force, force[-1])
 
-
-def compute_pile_tip_resistance(ptl, qc, depth, d_eq, alpha, beta, s, A, return_q_components=False):
-    """
-
-    Parameters
-    ----------
-    ptl : float
-        Pile tip level
-    qc : array
-        CPT's qc values
-    depth : array
-        Depth of cpt w/ respect to ground level
-    d_eq : float
-        Equivalent diameter pile.
-    alpha : float
-    beta : float
-    s : float
-    A : float
-        Pile tip area.
-    return_q_components : bool
-        Return separate qI, qII, qIII tracks
-
-    Returns
-    -------
-    rb : float
-        Resistance at pile tip.
-
-    """
-    d07 = 0.7 * d_eq + ptl
-    d4 = 4 * d_eq + ptl
-    d8 = ptl - 8 * d_eq
-
-    # closest indices
-    ppni = np.argmin(np.abs(ptl - depth))
-    d07i = np.argmin(np.abs(d07 - depth))
-    d4i = np.argmin(np.abs(d4 - depth))
-    d8i = np.argmin(np.abs(d8 - depth))
-
-    # range from ppn to 4d
-    qc_range_ppn_4d = qc[ppni:d4i + 1]
-
-    # cumulative min from 4d back to ppn
-    qc2_range = np.fmin.accumulate(qc_range_ppn_4d[::-1])[::-1]
-
-    # array used for determining the minimal rb-max. The minimal mean of both q1 and q2 are decisive
-    qc_calc = qc2_range + qc_range_ppn_4d
-    qc_calc_mean_range = qc_calc.cumsum() / np.arange(1, qc_calc.shape[0] + 1)
-
-    # index of the minimal mean of both qc1 and qc2
-    di = d07i - ppni
-
-    min_idx = np.argmin(qc_calc_mean_range[di:])
-    idx = min_idx + di if min_idx > 0 else min_idx + di + 1
-
-    # now the actual mean can be determined
-    qc1 = np.mean(qc_range_ppn_4d[:idx])
-    qc2 = np.mean(qc2_range[:idx])
-
-    # qc3 starts where qc2 is ended and accumulates the minimum again.
-    qc3_range = qc[d8i: ppni + 1].copy()
-    qc3_range[-1] = qc2_range[0]
-    qc3_range = np.fmin.accumulate(qc3_range[::-1])[::-1]
-    qc3 = np.mean(qc3_range)
-
-    rb = min(alpha * beta * s * 0.5 * (0.5 * (qc1 + qc2) + qc3), 15) * A
-
-    if return_q_components:
-        return rb, qc1, qc2, qc3
-    return rb
 
