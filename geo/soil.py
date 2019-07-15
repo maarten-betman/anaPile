84758f9b8a23f9a8761a081b9d0425147c47217c
ritchie46 <ritchie46@gmail.com>
2019-07-15 08:54:09 +0200

    docstring grain pressure

--- geo/soil.py
+++ geo/soil.py
@@ -2,9 +2,27 @@ import numpy as np
 
 
 def grain_pressure(depth, gamma_sat, u2=None):
+    """
+    Determine the grain pressure over the depth.
+
+    Parameters
+    ----------
+    depth : array
+        Depth values in [m], returned by Pygef.
+    gamma_sat : array
+        Saturated gamma values in [MPa]. Determined by classification.
+    u2 : array
+        Water pressure in [MPa]
+    Returns
+    -------
+    grain_pressure : array
+        Grain pressure in [MPa]
+    """
     h = np.diff(depth)
     h = np.append(h, h[-1])
     if u2 is None:
         u2 = depth * 0.01
 
     return np.cumsum(h * gamma_sat) - u2
+
+
