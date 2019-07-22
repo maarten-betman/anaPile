a027ac9e2c81a5129848b933978459ceeba1c322
ritchie46 <ritchie46@gmail.com>
2019-07-22 09:25:58 +0200

    fix plotting example w/ new pile class

--- examples/plot_range.py
+++ examples/plot_range.py
@@ -1,60 +1,28 @@
-from anapile.pressure.compose import single_pile
+from anapile.pressure.compose import PileCalculationLowerBound
 import pandas as pd
 import numpy as np
 from pygef import ParseGEF
-import matplotlib.pyplot as plt
 
 # read cpt and layers
 gef = ParseGEF('../tests/files/example.gef')
 gef.groundwater_level = 1
 layer_table = pd.read_csv('../tests/files/layer_table.csv')
 
-pile_tip_levels = np.linspace(5, gef.df.depth.max(), 20)
-
 pile_width = 0.25
 circum = pile_width * 4
 area = pile_width**2
 d_eq = 1.13 * pile_width
+alpha_p = 0.7
+beta_p = 1
+pile_factor_s = 1
 
-rb = []
-rs = []
-negative_friction = []
-ptls = []
-for ptl in pile_tip_levels:
-    try:
-        rb_, rs_, negative_friction_ = single_pile(gef, layer_table, ptl, d_eq, circum, area)
-        rb.append(rb_)
-        rs.append(rs_)
-        negative_friction.append(negative_friction_)
-        ptls.append(ptl)
+# plot single calculation
+cal = PileCalculationLowerBound(gef, d_eq, circum, area, layer_table)
+cal.plot_pile_calculation(pile_tip_level=8, show=True)
 
-    except IndexError:
-        pass
-
-rb = np.array(rb)
-rs = np.array(rs)
-negative_friction = np.array(negative_friction)
-
-fig = plt.figure(figsize=(8, 12))
-y_lim = [-gef.df.depth.max(), -gef.df.depth.min()]
-ax = fig.add_subplot(1, 3, 1)
-y = -gef.df.depth.values
-ax.plot(gef.df.qc.values, y)
-ax.set_ylim(y_lim)
-plt.grid()
-
-ax = fig.add_subplot(1, 3, 2)
-ax.plot(gef.df.friction_number.values, y)
-ax.set_ylim(y_lim)
-plt.grid()
-
-y = -np.array(ptls)
-ax = fig.add_subplot(1, 3, 3, label='rb')
-ax.plot(rb, y, label='rb')
-ax.plot(rs, y, label='rs')
-ax.plot(negative_friction, y, label='negative_friction')
-ax.plot(rb + rs - negative_friction, y, label=r'$R_{,cal}$')
-ax.set_ylim(y_lim)
-plt.grid()
-plt.legend(loc='upper right')
-plt.show()
+# plot range
+ptl_range = np.linspace(-5, gef.df.elevation_with_respect_to_NAP.min(), num=20)
+cal.plot_pile_calculation_range(
+    pile_tip_level=np.linspace(-3, gef.df.elevation_with_respect_to_NAP.min(), num=20),
+    show=True
+)
