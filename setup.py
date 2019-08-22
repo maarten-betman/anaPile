bab87b57dea95d97170c39b4f95df08aa1756ab0
ritchie46 <ritchie46@gmail.com>
2019-08-22 13:33:41 +0200

    update setup.py

--- setup.py
+++ setup.py
@@ -9,6 +9,6 @@ setup(
     url="https://ritchievink.com",
     download_url="https://github.com/ritchie46/anaPile",
     license="GPL-3.0",
-    packages=["anapile", "anapile.geo", "anapile.pressure", "anapile.settlement"],
-    install_requires=["matplotlib>=3.1", "numpy>=1.16", "scipy==1.3.0"],
+    packages=["anapile", "anapile.geo", "anapile.pressure", "anapile.settlement", "anapile.pressure.group"],
+    install_requires=["matplotlib>=3.1", "numpy>=1.16", "scipy==1.3.0", "sklearn"],
 )
